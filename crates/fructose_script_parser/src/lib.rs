9b0dde18fc6645b3a20740e701809d8ce0ff54ba
uzmoi <rizzzse@gmail.com>
2025-02-25 17:49:59 +0900

    add lalrpop

--- crates/fructose_script_parser/src/lib.rs
+++ crates/fructose_script_parser/src/lib.rs
@@ -1,5 +1,10 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use lalrpop_util::lalrpop_mod;
+
+lalrpop_mod!(grammar);
+
+pub fn parse(source: &str) -> Result<i32, Box<dyn std::error::Error + '_>> {
+    let a = grammar::FructoseScriptParser::new().parse(source)?;
+    Ok(a)
 }
 
 #[cfg(test)]
@@ -8,7 +13,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = parse("0");
+        assert_eq!(result.unwrap(), 0);
     }
 }
