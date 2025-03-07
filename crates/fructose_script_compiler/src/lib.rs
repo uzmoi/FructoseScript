7d1d40b9ff9acfa138ac1763b982dc975f7db8f0
uzmoi <rizzzse@gmail.com>
2025-03-07 22:07:43 +0900

    add JsGenerator

--- crates/fructose_script_compiler/src/lib.rs
+++ crates/fructose_script_compiler/src/lib.rs
@@ -1,4 +1,5 @@
 mod scope;
+mod transpile;
 
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
---
+++ crates/fructose_script_compiler/src/transpile.rs
@@ -0,0 +1,158 @@
+use std::{cell::Cell, ops::Range};
+
+use fructose_script_parser::{Visit, ast};
+use oxc::{
+    allocator::{Allocator, FromIn},
+    ast::ast::{self as oxc_ast, Expression, Statement},
+    span::{Atom, Span},
+};
+
+struct Block<'a> {
+    statements: oxc::allocator::Vec<'a, Statement<'a>>,
+}
+
+impl<'a> Block<'a> {
+    #[inline]
+    fn new(allocator: &'a Allocator) -> Self {
+        Self {
+            statements: oxc::allocator::Vec::new_in(allocator),
+        }
+    }
+
+    #[inline]
+    fn append_statement(&mut self, statement: Statement<'a>) {
+        self.statements.push(statement);
+    }
+}
+
+pub struct JsGenerator<'a> {
+    allocator: &'a Allocator,
+    current_block: Option<Block<'a>>,
+}
+
+impl<'a> JsGenerator<'a> {
+    #[inline]
+    pub fn new(allocator: &'a Allocator) -> Self {
+        Self {
+            allocator,
+            current_block: None,
+        }
+    }
+
+    #[inline]
+    fn boxed<T>(&self, value: T) -> oxc::allocator::Box<'a, T> {
+        oxc::allocator::Box::new_in(value, self.allocator)
+    }
+    #[inline]
+    fn atom(&self, name: &str) -> Atom<'a> {
+        Atom::from_in(name, self.allocator)
+    }
+}
+
+#[inline]
+fn span(range: &Range<usize>) -> Span {
+    Span::new(range.start as u32, range.end as u32)
+}
+
+impl<'a> Visit for JsGenerator<'a> {
+    fn visit_let(&mut self, node: &ast::Let) {
+        use oxc_ast::{
+            BindingIdentifier, BindingPattern, BindingPatternKind, VariableDeclaration,
+            VariableDeclarationKind, VariableDeclarator,
+        };
+
+        let value = self.visit_expression(&node.init);
+
+        let mut declarations = oxc::allocator::Vec::new_in(self.allocator);
+        let binding_identifier = self.boxed(BindingIdentifier {
+            span: span(&node.name.range),
+            name: self.atom(&node.name.value),
+            symbol_id: Cell::new(None),
+        });
+
+        let span = span(&node.range);
+
+        declarations.push(VariableDeclarator {
+            span,
+            kind: VariableDeclarationKind::Let,
+            id: BindingPattern {
+                kind: BindingPatternKind::BindingIdentifier(binding_identifier),
+                type_annotation: None,
+                optional: false,
+            },
+            init: value,
+            definite: false,
+        });
+
+        let statement = self.boxed(VariableDeclaration {
+            span,
+            kind: VariableDeclarationKind::Let,
+            declarations,
+            declare: false,
+        });
+
+        let current_block = self.current_block.as_mut().unwrap();
+        current_block.append_statement(Statement::VariableDeclaration(statement));
+    }
+
+    fn visit_assign(&mut self, node: &ast::Assign) {
+        use oxc_ast::{AssignmentExpression, AssignmentOperator, AssignmentTarget};
+
+        let target = self.boxed(oxc_ast::IdentifierReference {
+            span: span(&node.range),
+            name: self.atom(&node.target.value),
+            reference_id: Cell::new(None),
+        });
+        let value = self.visit_expression(&node.value);
+
+        let assignment = self.boxed(AssignmentExpression {
+            span: span(&node.range),
+            operator: AssignmentOperator::Assign,
+            left: AssignmentTarget::AssignmentTargetIdentifier(target),
+            right: value.unwrap(),
+        });
+
+        let statement = self.boxed(oxc_ast::ExpressionStatement {
+            span: span(&node.range),
+            expression: Expression::AssignmentExpression(assignment),
+        });
+
+        let current_block = self.current_block.as_mut().unwrap();
+        current_block.append_statement(Statement::ExpressionStatement(statement));
+    }
+
+    type Result = Option<Expression<'a>>;
+
+    fn visit_ident(&mut self, node: &ast::Ident) -> Self::Result {
+        let identifier = self.boxed(oxc_ast::IdentifierReference {
+            span: span(&node.range),
+            name: self.atom(&node.value),
+            reference_id: Cell::new(None),
+        });
+
+        Some(Expression::Identifier(identifier))
+    }
+
+    fn visit_nat_literal(&mut self, node: &ast::NatLiteral) -> Self::Result {
+        use oxc_ast::{NumberBase, NumericLiteral};
+
+        let numeric_literal = self.boxed(NumericLiteral {
+            span: span(&node.range),
+            value: node.value as f64,
+            raw: None,
+            base: NumberBase::Decimal,
+        });
+
+        Some(Expression::NumericLiteral(numeric_literal))
+    }
+
+    fn visit_block(&mut self, node: &ast::Block) -> Self::Result {
+        for statement in &node.statements {
+            self.visit_statement(statement);
+        }
+
+        node.last
+            .as_ref()
+            .and_then(|last| self.visit_expression(last))
+    }
+}
